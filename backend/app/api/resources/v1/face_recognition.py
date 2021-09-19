4560f2f90b11437083a1ecbe56fac57bc5eebf6a
Duy Nguyen <duynguyenngoc@hotmail.com>
2021-09-19 20:01:28 +0700

    add face detection onnxruntime

--- backend/app/api/resources/v1/face_recognition.py
+++ backend/app/api/resources/v1/face_recognition.py
@@ -3,11 +3,21 @@
 """
 
 from fastapi import APIRouter
-
+from worker.ml import face_detection
+import cv2
+import numpy as np
+from PIL import Image
 
 router = APIRouter()
 
 
-@router.post("/predict")
-def face_recognition():
-    pass
+@router.post("/detection")
+def face_detection_test():
+    image = cv2.imread('./worker/insightface/data/images/t1.jpg')
+    faces = face_detection.get(image)
+    for i in range(len(faces)):
+        face = faces[i].bbox.astype(np.int)
+        crop = face_detection.crop_image(image, face)
+        im = Image.fromarray(np.uint8(crop)).convert('RGB')
+        im = im.resize((224,224))
+        im.save('./api/resources/v1/{}.jpg'.format(i))
--- backend/app/worker/face_detection.py
+++ backend/app/worker/face_detection.py
@@ -21,45 +21,23 @@ class FaceDetection():
         **kwargs
     ):
         onnxruntime.set_default_logger_severity(3)
-        self.models = {}
+        self.models = None
         self.model_dir = ensure_available('models', name, root=root)
         onnx_files = glob.glob(osp.join(self.model_dir, '*.onnx'))
-        onnx_files = sorted(onnx_files)
-        for onnx_file in onnx_files:
-            print(onnx_file)
-            if onnx_file.find('_selfgen_')>0:
-                #print('ignore:', onnx_file)
-                continue
-            model = model_zoo.get_model(onnx_file, **kwargs)
-
-            if model is None:
-                print('model not recognized:', onnx_file)
-            elif allowed_modules is not None and model.taskname not in allowed_modules:
-                print('model ignore:', onnx_file, model.taskname)
-                del model
-            elif model.taskname not in self.models and (allowed_modules is None or model.taskname in allowed_modules):
-                print('find model:', onnx_file, model.taskname, model.input_shape, model.input_mean, model.input_std)
-                self.models[model.taskname] = model
-            else:
-                print('duplicated model task type, ignore:', onnx_file, model.taskname)
-                del model
-        assert 'detection' in self.models
-        self.det_model = self.models['detection']
-
+        onnx_file = onnx_files[0]
+        self.model = model_zoo.get_model(onnx_file, **kwargs)
+      
 
     def prepare(self, ctx_id, det_thresh=0.5, det_size=(640, 640)):
         self.det_thresh = det_thresh
         assert det_size is not None
         print('set det-size:', det_size)
         self.det_size = det_size
-        for taskname, model in self.models.items():
-            if taskname=='detection':
-                model.prepare(ctx_id, input_size=det_size, det_thresh=det_thresh)
-            else:
-                model.prepare(ctx_id)
+        self.model.prepare(ctx_id, input_size=det_size, det_thresh=det_thresh)
+
 
     def get(self, img, max_num=0):
-        bboxes, kpss = self.det_model.detect(img,
+        bboxes, kpss = self.model.detect(img,
                                              max_num=max_num,
                                              metric='default')
         if bboxes.shape[0] == 0:
@@ -72,10 +50,6 @@ class FaceDetection():
             if kpss is not None:
                 kps = kpss[i]
             face = Face(bbox=bbox, kps=kps, det_score=det_score)
-            for taskname, model in self.models.items():
-                if taskname=='detection':
-                    continue
-                model.get(img, face)
             ret.append(face)
         return ret
 
